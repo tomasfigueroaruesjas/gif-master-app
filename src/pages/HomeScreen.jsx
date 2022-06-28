83a43d03072f6567ab641c17ba2ba8513f0b832f
tomas figueroa <tomasfigueroaruesjas@gmail.com>
2022-06-28 14:08:59 -0300

    Agregar stash

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import banner from "../assets/banner.gif";
 
 const HomeScreen = () => {
   return (
-    <div>
-      <h1>Home Screen</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div className="w-100">
+            <img className="img_banner" src={banner} alt="banner" />
+          </div>
+        </div>
+      </div>
+      <div className="row mt-5 ">
+        <div className="col">
+          <h3 className="text-white">
+            <i className="fa fa-line-chart" aria-hidden="true"></i> Trending
+          </h3>
+        </div>
+      </div>
+
+      <div className=" galeria">{/* Imagenes de gifs  */}</div>
     </div>
   );
 };
