e1a49bd1d3d161a14a2799e3f1f1ffcbfad96591
tomas figueroa <tomasfigueroaruesjas@gmail.com>
2022-07-07 15:13:18 -0300

    getGifsearch e InfoCategory

--- src/components/InfoCategory.jsx
+++ src/components/InfoCategory.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-const InfoCategory = () => {
+const InfoCategory = ({datos}) => {
+
   return (
     <div className="col-3  d-flex flex-column align-items-center animate__animated animate__fadeIn">
       <div className="img_col">
-        <img src="" alt="" className="img_paginas" />
+        <img src={datos.imagen} alt={datos.categoria} className="img_paginas" />
       </div>
       <div className="texto_small mt-3 text-muted mx-5">
-        <p>texto</p>
+        <p>{datos.texto}</p>
         <p className="fw-bold">Follow on:</p>
         <div className="text-white text-start w-100 d-flex justify-content-start">
           <div>
--- src/pages/CategoryScreen.jsx
+++ src/pages/CategoryScreen.jsx
@@ -1,15 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import InfoCategory from '../components/InfoCategory'
+import { getGifSearch } from '../helpers/fetchApi'
 import { useParams } from 'react-router-dom';
 
 const CategoryScreen = () => {
   const {id} = useParams();
+  let datos = {};
+  const [cantidad, setCantidad] = useState(18)
+
+  useEffect(() => {
+    getGifSearch(id, 18).then(resp => {
+      let arr = [];
+      resp.forEach((element) => {
+        const { title, id } = element;
+        const { url } = element.image.original
+        arr.push({title, id, url})
+      })
+
+    })
+  }, [])
+
+
+  switch(id) {
+    case "entertainment":
+      datos = {
+        categoria: "Entertainment",
+        imagen:
+          "https://media.giphy.com/avatars/entertainment/Poc7fb6dD9q5/200h.gif",
+        texto: "Get the latest GIFs from movies, TV, music, celebrities.",
+      };
+      break;
+
+    case "sports":
+      datos = {
+        categoria: "Sports",
+        imagen:
+          "https://media.giphy.com/channel_assets/sports/P658KMA9mwy4/200h.gif",
+        texto:
+          "Your go-to for any and all sports GIFs! LeBron, Ronaldo, Gronk, Serena...We've got them all!",
+      };
+      break;
+    case "reactions":
+      datos = {
+        categoria: "Reactions",
+        imagen:
+          "https://media.giphy.com/channel_assets/reactions/k2ybPvSfRQuK/200h.gif",
+        texto: "Don't tell it to me, GIF it to me!",
+      };
+      break;
+  }
+
   console.log(id)
   return (
     <div className="container">
       <div className="row">
         {/* Información de categorias  */}
-        <InfoCategory />
+        <InfoCategory datos={datos} />
         {/* Tarjetas de giofs  */}
       </div>
     </div>
